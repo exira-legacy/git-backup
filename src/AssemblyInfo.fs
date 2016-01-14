c893040bc7ad6efc5d8ec052380b7e9c4f1b195b
Team City <info@exira.com>
2016-01-14 22:45:23 +0000

    Bump version to 0.5.7

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("git-backup")>]
 [<assembly: AssemblyProductAttribute("Exira.GitBackup")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.GitBackup is a console application which backs up Git repositories on a rotating schedule")>]
-[<assembly: AssemblyVersionAttribute("0.4.6")>]
-[<assembly: AssemblyFileVersionAttribute("0.4.6")>]
-[<assembly: AssemblyMetadataAttribute("githash","2cc5ed25dbf74a86cebec7159efe6fb8b5919755")>]
+[<assembly: AssemblyVersionAttribute("0.5.7")>]
+[<assembly: AssemblyFileVersionAttribute("0.5.7")>]
+[<assembly: AssemblyMetadataAttribute("githash","6e7088b5aa5de027d7f16cce96e42d77914a6c80")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.4.6"
+    let [<Literal>] Version = "0.5.7"
